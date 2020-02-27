120964fd92801f5c347c52d1e3dd7738ac077f90
nicolas <zeissern@gmail.com>
2020-02-27 09:20:17 +0100

    Added a new model favourites to save user's imagier

--- imagier/models.py
+++ imagier/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Category(models.Model):
     name = models.CharField(max_length=30, unique=True, null=False)
@@ -9,12 +11,19 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-
 class Item(models.Model):
     name = models.CharField(max_length=30, unique=True, null=False)
     picture = models.URLField(blank=True, null=False)
     category = models.ManyToManyField(Category, related_name='item', blank=True)
+    user = models.ManyToManyField(User, related_name='item', blank=True)
 
     def __str__(self):
         return self.name
 
+class Favourites(models.Model):
+    name = models.CharField(max_length=30, unique=True, null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    item = models.ManyToManyField(Item, related_name='item')
+
+    def __str__(self):
+        return self.name
