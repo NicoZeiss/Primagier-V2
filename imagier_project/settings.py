1a22d2b4cd3abccf19d0c22cb77311dfa4ee59cb
nicolas <zeissern@gmail.com>
2020-03-14 02:30:45 +0100

    need to modify settings allowed host to host

--- imagier_project/settings.py
+++ imagier_project/settings.py
@@ -30,7 +30,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = ['test-imagier.herokuapp.com']
+ALLOWED_HOSTS = ['NOMDOMAINE.herokuapp.com']
 
 
 # Application definition
