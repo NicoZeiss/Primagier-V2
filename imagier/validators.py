c26f83dde4fb510cdb503c327b46a19c5c2306e1
nicolas <zeissern@gmail.com>
2020-03-17 19:16:42 +0100

    fixed fonts issue when rendering pdf template

--- imagier/validators.py
+++ imagier/validators.py
@@ -14,10 +14,6 @@ def verify_image_url(value):
     if not mimetype:
         if not any(extension in value for extension in valid_image_extensions):
             raise ValidationError('URL non conforme : merci de renseigner un URL vers une image')
-    # try:
-    #     value.encode('ascii', 'strict')
-    # except UnicodeEncodeError:
-    #     raise ValidationError('URL non conforme')
 
 def verify_image_not_in_db(value):
     """Verify if the picture url already exists into db"""
